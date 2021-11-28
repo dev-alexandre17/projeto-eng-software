9acfa1261c9359301b1258dc9114def9588dbc16
dev-alexandre17 <alexandretec77@gmail.com>
2021-11-28 00:22:17 -0300

    Modificando arquivo

--- login.php
+++ login.php
@@ -1,58 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/formulario.css" media="screen"/>
-    <title> Cadastro </title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/Loginn.css">
+    <link rel="shortcut icon" href="Imagens/favicon.ico" type="image/x-icon">
 </head>
 <body>
-    <section class = "formulario">
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <div class="box-container">
-        <legend>Cadatre-se gratuitamente </legend>
-          <br><br>
-            <div class="inputBox">
-              <input type="text" name="name" id="nome" class="inputUser" required>
-              <label for="nome" class="labelInput">Nome</label>
-            </div>
-            <br><br>
-            <div class="inputBox">
-              <input type="text" name="email" id="email" class="inputUser" required>
-              <label for="email" class="labelInput">Email</label>
-            </div>
-            <br><br>
-            <div class="inputBox">
-              <input type="text" name="password" id="senha" class="inputUser" required>
-              <label for="senha" class="labelInput">Senha</label>
-            </div>
-            <br><br>
-            <div class="inputBox">
-                <input type="tel" name="phoneNumber" id="telefone" class="inputUser" required>
-                <label for="telefone" class="labelInput">Telefone</label>
-            </div>
-            <br>
-            <input type="submit" name="enviar" id="submit" value="Send">
+    <main>
+        <h1>Entrar</h1>
+        <div class="social-media">
+            <a target="Google" href="https://www.google.com.br/">
+                <img src="Imagens/ImagensLogin/google.png" alt="Google">
+            </a>
+            <a target="facebook" href="https://pt-br.facebook.com/login.php/">
+                <img src="Imagens/ImagensLogin/facebook.png" alt="Facebook">
+            </a>
+            <a target="Linkedin" href="https://www.linkedin.com/login/pt">
+                <img src="Imagens/ImagensLogin/linkedin.png" alt="Linkedin">
+            </a>
         </div>
-      </form>
-    </section>
-    <section class="imagem-formulario">
-        <div class="imagem">
-            <img src="https://images.pexels.com/photos/2667528/pexels-photo-2667528.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="mulher-preta">
+
+        <div class="alternative">
+            <span>ou</span>
         </div>
+
+        <form action="">
+
+            <label for="email">
+                <span>E-mail</span>
+                <input type="email" name="password" placeholder="Digite seu E-mail">
+            </label>
+
+            <label for="password">
+                <span>Senha</span>
+                <input type="password" name="password" placeholder="Digite sua Senha" required>
+            </label>
+            <a target="*" href="Cadastro.html">Cadastrar</a>
+
+            <button type="submit" ><a href="index.html"> Entrar </a></button> 
+        </form>
+    </main>
+    <section class="images">
+        <img src="Imagens/ImagensLogin/dslr-camera.png" alt="foto">
+        <div class="circle"></div>
     </section>
+    
 </body>
 </html>
 <?php
-  $user_name = $_POST["name"];
-  $user_email = $_POST["email"];
-  $user_password = $_POST["password"];
-  $user_phone_numer = $_POST["phoneNumber"];
-  if ($user_name == "admin" && $user_email == "admin123@gmail.com" && $user_password == "123" &&
-  $user_phone_numer == "(81) 99999-9999") {
-    print "<script> type='text/javascript'>alert('Dados válidados com sucesso!');</script>";
-  } else { 
-    print "<script> type='text/javascript'>alert('Dados inválidos. Tente novamente mais tarde.');</script>";
+  if (isset($_POST['nome']) && ($_POST['email'])) {
+    echo "Dados válidados.";
   }
 ?>
